8a16aef8bdd6015d773c93e6ed3737032c34dbf1
54607-24-aunchittha <54607@yupparaj.ac.th>
2026-09-07 09:44:41 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -101,4 +101,4 @@ if st.session_state.get("is_ended", False):
     show_result_dialog(ans1, ans2)
 
 st.divider()
-st.writest("นางสาวอํญชิษฐา คงโพธิ์ทอง เลขที่ 24 ม.4/4")
+st.write("นางสาวอํญชิษฐา คงโพธิ์ทอง เลขที่ 24 ม.4/4")
